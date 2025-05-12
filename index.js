27aae0db042c608d1ba4723f98cd99087b73fbc2
Gratian Dicu <gratian.dicu@softvision.com>
2025-05-12 12:26:14 +0300

    Initialize package.json with project metadata and dependencies

---
+++ index.js
@@ -0,0 +1,27 @@
+import { select, input } from '@inquirer/prompts';
+import { exec } from 'child_process';
+import * as util from 'util';
+
+
+const execPromise = util.promisify(exec);
+
+const files = await execPromise('xcrun simctl list |  grep \'Booted\'');
+const devices = files.stdout.split('\n').map(line=> line.trim()).filter(line => line.length > 0).map(line => {
+    const parts = line.split('(');
+    const deviceName = parts[0].trim();
+    const id = parts[1].trim().split(')')[0].trim();
+    return { name: deviceName, value: id };
+}
+);
+const selectedDeviceID = await select({ message: 'Select the simulator',  choices: devices});
+
+const link = await input({ message: 'Enter your link' });
+
+const result = await execPromise(`xcrun simctl openurl ${selectedDeviceID} "${link}"`);
+console.warn(result.stdout);
+console.error(result.stderr);
+if (result.error) {
+    console.error('Error:', result.error);
+}
+console.log('Done');
+
