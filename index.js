760f9d443fdcbbc3cad7f5e9f478bf1707ba8581
Gratian Dicu <gratian.dicu@softvision.com>
2025-05-21 16:48:09 +0300

    Fix device ID extraction logic in index.js for simulator selection (e.g. iphone SE(3rd generation)

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const execPromise = util.promisify(exec);
 const files = await execPromise('xcrun simctl list |  grep \'Booted\'');
 const devices = files.stdout.split('\n').map(line=> line.trim()).filter(line => line.length > 0).map(line => {
     const parts = line.split('(');
+    const partsLength = parts.length;
     const deviceName = parts[0].trim();
-    const id = parts[1].trim().split(')')[0].trim();
-    return { name: deviceName, value: id };
+    const id = parts[partsLength - 2].trim().split(')')[0].trim();
+    const output = { name: deviceName, value: id };
+    return output
 }
 );
 const selectedDeviceID = await select({ message: 'Select the simulator',  choices: devices});
